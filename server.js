b9538da191214657f215705580ab41ad75cde791
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2022-03-24 00:42:11 +0530

    jwt authentication added for login

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 const path = require("path");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcryptjs");
-const user = require("./model/user");
+const jwt = require("jsonwebtoken");
+const User = require("./model/user");
 
 // configuration
 mongoose
@@ -19,10 +20,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use("/", express.static(path.join(__dirname, "static")));
 
+// User registration
 app.post("/api/register", async (req, res) => {
   console.log(req.body);
 
-  // password hashing
   const { username, password: plainTextPassword } = req.body;
   if (!username || typeof username !== "string") {
     return res.json({ status: "error", error: "Invalid username" });
@@ -38,11 +39,11 @@ app.post("/api/register", async (req, res) => {
       error: "password should be more than 6 character",
     });
   }
-
+  // password hashing
   const password = await bcrypt.hash(plainTextPassword, 10);
 
   try {
-    const response = await user.create({
+    const response = await User.create({
       username,
       password,
     });
@@ -58,7 +59,26 @@ app.post("/api/register", async (req, res) => {
   res.json({ status: "ok" });
 });
 
+app.post("/api/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username }).lean();
 
+  if (!user) {
+    return res.json({ status: "error", error: "Invalid username or password" });
+  }
+
+  if (await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign(
+      {
+        id: user._id,
+        username: user.username,
+      },
+      process.env.JWT_SECRET
+    );
+    return res.json({ status: "ok", data: token });
+  }
+  res.json({ status: "error", error: "Invalid username or password" });
+});
 
 // listening on port
 const port = process.env.PORT || 8000;
