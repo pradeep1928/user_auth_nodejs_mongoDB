b6e22cdb7804b871e402141419950a91ae063608
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2022-03-24 02:19:00 +0530

    changepassword page created with jwt authenctication

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("./model/user");
+const { route } = require("express/lib/application");
 
 // configuration
 mongoose
@@ -59,6 +60,8 @@ app.post("/api/register", async (req, res) => {
   res.json({ status: "ok" });
 });
 
+
+// login route
 app.post("/api/login", async (req, res) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username }).lean();
@@ -79,6 +82,43 @@ app.post("/api/login", async (req, res) => {
   }
   res.json({ status: "error", error: "Invalid username or password" });
 });
+
+
+
+// Change password route
+app.post("/api/changepassword", async (req, res) => {
+  const { token, newpassword: plainTextPassword } = req.body;
+
+  if (!plainTextPassword || typeof plainTextPassword !== "string") {
+    return res.json({ status: "error", error: "Invalid password" });
+  }
+
+  if (plainTextPassword.length < 6) {
+    return res.json({
+      status: "error",
+      error: "password should be more than 6 character",
+    });
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(user)
+    const _id = user.id;
+    console.log(_id);
+    const password = await bcrypt.hash(plainTextPassword, 10);
+    const newUser = await User.updateOne(
+      { _id },
+      {
+        $set: { password },
+      }
+    )
+    res.json({status: 'ok'});
+  } catch (error) {
+    res.json({ status: "error", error: "something went wrong" });
+  }
+});
+
+
 
 // listening on port
 const port = process.env.PORT || 8000;
